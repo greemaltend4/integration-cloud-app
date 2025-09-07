aa4a277ce362bc28b98241ca576f5107b946f32f
greemaltend4 <dengestabiy@hotmail.com>
2025-09-07 14:28:42 -0500

    Fix typo in welcome message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Integration Cloud App!');
+  res.send('Welcome to the Integration Cloud Application!');
 });
 
 app.listen(PORT, () => {
